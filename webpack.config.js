fdb23f1ea2981fb1219a6a08ba549f23253d2e6b
Takenori Oshima <takenorioshima@gmail.com>
2022-05-19 13:07:00 +0900

    update: Add glb support

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/entry.ts',
   output: {
-    path: `${__dirname}/dist`,
-    filename: '[name].[fullhash].js'
+    filename: '[name].[fullhash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader'
-      },
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/models/*.glb',
+          to({ context, absoluteFilename }) {
+            return "models/[name][ext]";
+          }
+        }
+      ]
+    }),
   ],
   resolve: {
     extensions: ['.ts', '.js']
+  },
+  devServer: {
+    open: true,
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    }
   }
 };

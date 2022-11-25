20eddc966af05ce8be96eddc7d1f2fb21664e045
Takenori Oshima <takenorioshima@gmail.com>
2022-11-25 15:51:14 +0900

    tweak: Tweak camera position

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Takenori from './Takenori';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -22,10 +22,10 @@ const cube = new THREE.Mesh(geometry, material);
 cube.material.wireframe = true;
 scene.add(cube);
 
-camera.position.z = 5;
 const takenori = new Takenori();
 scene.add(takenori);
 
+camera.position.z = 2;
 
 const tick = (): void => {
     requestAnimationFrame(tick);

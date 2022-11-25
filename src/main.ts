e3f7a2f5b60ff5eba3a14587a7b3f37e5e6c1e3a
Takenori Oshima <takenorioshima@gmail.com>
2022-11-25 15:56:31 +0900

    refactor: Add comments

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,42 @@ import Stats from 'three/examples/jsm/libs/stats.module.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Takenori from './Takenori';
 
+// Create scene.
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xffcf00 );
 
+// Create camera.
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 2;
 
-const stats = Stats();
-document.body.appendChild(stats.dom);
-
+// Initialize renderer.
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Show FPS stats, add orbit control.
+const stats = Stats();
+document.body.appendChild(stats.dom);
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Add lights.
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
 scene.add(ambientLight);
 scene.add( directionalLight );
 
+// Add cube to scene.
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial();
 const cube = new THREE.Mesh(geometry, material);
 cube.material.wireframe = true;
 scene.add(cube);
 
+// Add glb model to scene.
 const takenori = new Takenori();
 scene.add(takenori);
 
-camera.position.z = 2;
-
+// Request animation frame.
 const tick = (): void => {
     requestAnimationFrame(tick);
 
@@ -48,6 +54,7 @@ const tick = (): void => {
 }
 tick();
 
+// Add window resize event.
 const windowResizeHanlder = (): void => {
     const width = window.innerWidth;
     const height = window.innerHeight;
